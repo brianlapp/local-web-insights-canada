521b5c753b40cd9f090044b800753ed2d3f0354b
brianlapp <brian.lapp@gmail.com>
2025-04-04 13:03:44 -0400

    routing error fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route, Outlet } from 'react-router-dom'
 import { Toaster } from '@/components/ui/toaster'
 import { Toaster as Sonner } from '@/components/ui/sonner'
 import { TooltipProvider } from '@/components/ui/tooltip'
@@ -34,16 +34,18 @@ function App() {
                 <Route path="login" element={<LoginPage />} />
                 <Route path="reset-password" element={<ResetPasswordPage />} />
                 <Route path="update-password" element={<UpdatePasswordPage />} />
-                <Route element={<ProtectedRoute>
-                  <AdminLayout>
-                    <Routes>
-                      <Route path="dashboard" element={<DashboardPage />} />
-                      <Route path="businesses" element={<BusinessList />} />
-                      <Route path="petitions" element={<PetitionList />} />
-                      <Route path="settings" element={<Settings />} />
-                    </Routes>
-                  </AdminLayout>
-                </ProtectedRoute>} />
+                <Route element={
+                  <ProtectedRoute>
+                    <AdminLayout>
+                      <Outlet />
+                    </AdminLayout>
+                  </ProtectedRoute>
+                }>
+                  <Route path="dashboard" element={<DashboardPage />} />
+                  <Route path="businesses" element={<BusinessList />} />
+                  <Route path="petitions" element={<PetitionList />} />
+                  <Route path="settings" element={<Settings />} />
+                </Route>
               </Route>
 
               {/* Public routes */}
